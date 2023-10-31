2b9e658ffe84b03f8d4c532387f642e1fef0372f
John P <70922864+p134c0d3@users.noreply.github.com>
2023-10-30 22:11:23 -0500

    Tour of Heroes Progress

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,24 @@
+import { HeroService } from './../hero.service';
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-  hero:Hero = {
-    id: 1,
-    name: 'Windstorm'
+  constructor(private HeroService: HeroService) {}
+  ngOnInit(): void {
+    this.getHeroes();
   }
-
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  getHeroes(): void {
+    this.heroes = this.HeroService.getHeroes();
+  }
 }
